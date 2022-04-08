0d015b83389f082a74cbd9b1cb3a61d11318349f
Beef Teriyaki <43371545+Renevua@users.noreply.github.com>
2022-04-08 23:35:10 +0100

    Fixing navigation bar

--- server.js
+++ server.js
@@ -48,8 +48,8 @@ app.post('/auth', function(request, response) {
 	let password = request.body.password;
 	// Ensure the input fields exists and are not empty
 	if (username && password) {
-		// Execute SQL query that'll select the account from the database based on the specified username and password
-		connection.query('SELECT * FROM accounts WHERE username = ? AND password = ?', [username, password], function(error, results, fields) {
+		// Execute DBMongo query that'll select the account from the database based on the specified username and password
+    db.collection('Logininfo').find({ "Username": username }).toArray(function (err, result) {
 			// If there is an issue with the query, output the error
 			if (error) throw error;
 			// If the account exists
