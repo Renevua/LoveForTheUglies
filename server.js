df37b776de9721731f915c55336640fda18dd472
Beef Teriyaki <43371545+Renevua@users.noreply.github.com>
2022-04-08 21:49:42 +0100

    Played around with navigation's css, adding login stuff

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser')
 const app = express();
 require('dotenv').config()
 const session = require('express-session');
-app.use(express.static('assets'))
+app.use(express.static('./assets'))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.set('view engine', 'ejs');
 var db;
@@ -22,4 +22,50 @@ MongoClient.connect(process.env.uri, function (err, client) {
 //Redirecting user to the correct index page
 app.get('/', function (req, res) {
   res.render('pages/index.ejs');
+});
+
+//Redirecting user to the Events
+app.get('/Events', function (req, res) {
+  res.render('pages/events.ejs');
+});
+
+//Redirecting user to the Login
+app.get('/Login', function (req, res) {
+  res.render('pages/login.ejs');
+});
+
+//Redirecting user to the Your Page
+app.get('/Yourpage', function (req, res) {
+  res.render('pages/yourpage.ejs');
+});
+
+//Login code for users to log in into website
+//Repurposed from the CodeShack
+// http://localhost:3000/auth
+app.post('/auth', function(request, response) {
+	// Capture the input fields
+	let username = request.body.username;
+	let password = request.body.password;
+	// Ensure the input fields exists and are not empty
+	if (username && password) {
+		// Execute SQL query that'll select the account from the database based on the specified username and password
+		connection.query('SELECT * FROM accounts WHERE username = ? AND password = ?', [username, password], function(error, results, fields) {
+			// If there is an issue with the query, output the error
+			if (error) throw error;
+			// If the account exists
+			if (results.length > 0) {
+				// Authenticate the user
+				request.session.loggedin = true;
+				request.session.username = username;
+				// Redirect to home page
+				response.redirect('/home');
+			} else {
+				response.send('Incorrect Username and/or Password!');
+			}			
+			response.end();
+		});
+	} else {
+		response.send('Please enter Username and Password!');
+		response.end();
+	}
 });
